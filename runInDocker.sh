5eaa586fcad259d4a184020f5ffba391b74fece8
gg3083 <g@1998.ws>
2019-07-24 21:55:15 +0800

    修改启动文件

--- runInDocker.sh
+++ runInDocker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [$1=='web'];then
+if [ $1 == 'web' ];then
  docker run -p 8443:8443 -t springboot/blog-web
-else if [$1=='admin'];then
+elif [ $1 == 'admin' ];then
  docker run -p 8001:8001 -t springboot/blog-admin
 else
- echo '参数有误！'
+ echo "参数有误！"
+fi
