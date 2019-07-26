259280f51368001d418c690fc308eb1ac483ede6
gimi <g@1998.ws>
2019-07-26 14:50:41 +0800

    修改配置环境代码

--- runInDocker.sh
+++ runInDocker.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 if [ $1 == 'web' ];then
- docker run -p 8443:8443 --name blog-web -it -v /root/git-rep/docker-jar/web/blog-web-1.0.0.jar:/app.jar --privileged=true -t springboot/blog-web
+ docker run --name blog-web  -p 8443:8443 -t springboot/blog-web
 elif [ $1 == 'admin' ];then
- docker run --name blog-admin  -it -v /root/git-rep/docker-jar:/data -p 8001:8001 springboot/blog-admin
-
+ docker run --name blog-admin  -p 8001:8001 -t springboot/blog-admin
 else
  echo "参数有误！"
 fi
